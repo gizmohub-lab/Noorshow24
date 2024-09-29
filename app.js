f96a329eb8d9ed775b6ac9d089b535f1a0177f80
gizmohub-lab <gizmo.hub.in@gmail.com>
2024-09-29 16:23:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require ('express')
 const app = express()
 
 const mongoose = require('mongoose');
-
+const multer = require('multer')
 const path = require('path');
 const Announcement = require('./models/Announcement')
 const bidayaModel = require('./models/bidayaModel');
